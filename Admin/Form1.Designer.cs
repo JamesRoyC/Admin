ea3732d2b21edf69287548c35e8dc24d7ae1d4f0
JamesRoyC <93945424+JamesRoyC@users.noreply.github.com>
2021-12-14 18:54:22 +0800

    Initial commit

---
+++ Admin/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Admin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_User = new System.Windows.Forms.TextBox();
+            this.txt_Pass = new System.Windows.Forms.TextBox();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.checkConnection = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(205, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PASS";
+            // 
+            // txt_User
+            // 
+            this.txt_User.Location = new System.Drawing.Point(285, 66);
+            this.txt_User.Name = "txt_User";
+            this.txt_User.Size = new System.Drawing.Size(100, 22);
+            this.txt_User.TabIndex = 2;
+            // 
+            // txt_Pass
+            // 
+            this.txt_Pass.Location = new System.Drawing.Point(285, 101);
+            this.txt_Pass.Name = "txt_Pass";
+            this.txt_Pass.Size = new System.Drawing.Size(100, 22);
+            this.txt_Pass.TabIndex = 3;
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(285, 170);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(113, 53);
+            this.btn_Login.TabIndex = 4;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // checkConnection
+            // 
+            this.checkConnection.AutoSize = true;
+            this.checkConnection.Location = new System.Drawing.Point(22, 321);
+            this.checkConnection.Name = "checkConnection";
+            this.checkConnection.Size = new System.Drawing.Size(46, 17);
+            this.checkConnection.TabIndex = 5;
+            this.checkConnection.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 483);
+            this.Controls.Add(this.checkConnection);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.txt_Pass);
+            this.Controls.Add(this.txt_User);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_User;
+        private System.Windows.Forms.TextBox txt_Pass;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Label checkConnection;
+    }
+}
+
---
+++ Admin/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Admin
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection connection = new OleDbConnection();
+        public Form1()
+        {
+            InitializeComponent();
+            connection.ConnectionString = @"
+                Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Admin\Documents\AdminDatabase.accdb;
+                Persist Security Info=False; ";                        
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                checkConnection.Text = "Connection Successful";
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = connection; 
+
+            command.CommandText = " select * from UserData where username='" + txt_User.Text + "' and password='"+txt_Pass.Text+"'";
+
+            OleDbDataReader reader = command.ExecuteReader();
+            int count = 0;
+            while (reader.Read())
+            {
+                count++;
+            }
+            if(count == 1)
+            {
+                MessageBox.Show("Username and password is correct ");
+            }
+            else if (count > 1)
+            {
+                MessageBox.Show("Duplicate username and pass");
+            }
+            else
+            {
+                MessageBox.Show("user pass not correct");
+            }
+            connection.Close();
+        }
+    }
+}
